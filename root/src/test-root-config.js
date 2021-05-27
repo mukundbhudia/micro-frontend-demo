deb24b8e377887eb192fe4dcb5d5d0b532f8a245
Mukund Bhudia <mukund_bhudia@hotmail.com>
2021-05-27 12:10:45 +0100

    feat: wired up and mounted page1

--- root/src/test-root-config.js
+++ root/src/test-root-config.js
@@ -1,19 +1,19 @@
 import { registerApplication, start } from "single-spa";
 
+// registerApplication({
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
+// });
+
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
+  name: "@test/page1",
+  app: () => System.import("@test/page1"),
   activeWhen: ["/"],
 });
-
-// registerApplication({
-//   name: "@test/navbar",
-//   app: () => System.import("@test/navbar"),
-//   activeWhen: ["/"]
-// });
 
 start({
   urlRerouteOnly: true,
